5b449cf4f729147e90c9ed11ec26fee07145435c
Olivier Lalonde <olalonde@gmail.com>
2016-03-22 16:14:40 -0700

    fix s3 bucket env

--- rootfs/scripts/backup-s3.sh
+++ rootfs/scripts/backup-s3.sh
@@ -3,6 +3,6 @@
 # TODO: list buckets and backup all of them
 
 mkdir -p /data/latest/s3
-echo "Starting aws s3 sync"
+echo "Starting aws s3 sync for bucket ${AWS_S3_BUCKET}"
 aws s3 sync "s3://${AWS_S3_BUCKET}" "/data/latest/s3/${AWS_S3_BUCKET}"
 echo "Completed aws s3 sync"
